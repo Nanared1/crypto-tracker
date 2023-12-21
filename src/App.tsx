d973f692ac251dafcfe96588afa5870c9358be1b
Nana Abekah <abekah12345@gmail.com>
2023-12-20 21:21:10 -0330

    remove most of the CRA boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import styled from "styled-components";
 
 const queryClient = new QueryClient()
+
+
+const S = {
+  Container: styled.div`
+    display: flex;
+    width: 100%;
+    height: 100vh;
+  `
+}
 
 
 const Chart = () => {
@@ -17,22 +25,9 @@ const Chart = () => {
 const  App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Chart />
-          </a>
-        </header>
-      </div>
+      <S.Container>
+      <Chart />
+      </S.Container>
     </QueryClientProvider>
   );
 }
